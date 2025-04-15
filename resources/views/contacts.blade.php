5b85db3b20206ae66fe23ab5b6f0145fb9d6f522
tome-21902652 <tome.alvim@gmail.com>
2025-04-15 19:27:26 +0100

    Create contacts

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -15,72 +15,68 @@
 </head>
 
 <body>
-  @include('navbar')
-  <main class="main">
-    <div class="container">
-      <section class="contact-section">
-        <div class="contact-grid">
-          <div class="contact-left">
-            <h1 class="title">Entre em contacto</h1>
-            <div class="company-name">
-              <span class="company-text">MEDINA MONTEIRO </span>
-              <span class="company-text">BENSIMON </span>
-              <span class="company-highlight">ADVOGADOS </span>
+  <div class="container">
+    @include('navbar-fixed')
+    <main class="main">
+      <div class="cont-grid">
+        <section class="contact-section">
+          <div class="contact-grid">
+            <div class="contact-left">
+              <h1 class="title">Entre em contacto</h1>
+              <div class="company-name">
+                <span class="company-text">MEDINA MONTEIRO </span>
+                <span class="company-text">BENSIMON </span>
+                <span class="company-highlight">ADVOGADOS </span>
+              </div>
+              <div class="map-container">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d389.1629926956701!2d-9.13997387553408!3d38.710830268706246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2spt!4v1744738483933!5m2!1sen!2spt"
+                  width="100%" height="320" style="border:0;" allowfullscreen="" loading="lazy"
+                  referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
+              </div>
             </div>
-            <div class="map-container">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d389.1629926956701!2d-9.13997387553408!3d38.710830268706246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2spt!4v1744738483933!5m2!1sen!2spt"
-                width="100%" height="320" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade">
-              </iframe> 
+            <grid>
+              <div class="contact-right">
+                <div class="contact-info">
+                  <div class="contact-item">
+                    <div class="icon">
+                      <img src="/icons/location-1.svg" alt="Location" />
+                    </div>
+                    <div class="info">
+                      <h3>MORADA</h3>
+                      <p>Rua Garret, n.º 19, 2ºC<br>1200-203 Lisboa, Portugal</p>
+                    </div>
+                  </div>
 
-              <!--
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3121.314070692973!2d-9.1422617!3d38.710846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd19331e0efc5f1b%3A0xb6579de5e351a2e2!2sLisbon!5e0!3m2!1sen!2spt!4v1611111111111"
-                width="100%" height="320px" style="border:0;" allowfullscreen="" loading="lazy">
-              </iframe> -->
-            </div>
-          </div>
-          <grid>
-            <div class="contact-right">
-              <div class="contact-info">
-                <div class="contact-item">
-                  <div class="icon">
-                    <img src="/icons/location-1.svg" alt="Location" />
+                  <div class="contact-item">
+                    <div class="icon">
+                      <img src="/icons/mail.svg" alt="Email" />
+                    </div>
+                    <div class="info">
+                      <h3>EMAIL</h3>
+                      <a href="mailto:MMBADVOGADOS@GMAIL.COM">geral@mmba.pt</a>
+                    </div>
                   </div>
-                  <div class="info">
-                    <h3>MORADA</h3>
-                    <p>Rua Garret, n.º 19, 2ºC<br>1200-203 Lisboa, Portugal</p>
-                  </div>
-                </div>
 
-                <div class="contact-item">
-                  <div class="icon">
-                    <img src="/icons/mail.svg" alt="Email" />
-                  </div>
-                  <div class="info">
-                    <h3>EMAIL</h3>
-                    <a href="mailto:MMBADVOGADOS@GMAIL.COM">geral@mmba.pt</a>
-                  </div>
-                </div>
-
-                <div class="contact-item">
-                  <div class="icon">
-                    <img src="/icons/phone-1.svg" alt="Phone" />
-                  </div>
-                  <div class="info">
-                    <h3>TELEFONE</h3>
-                    <p>(+351) 218 386 856</p>
+                  <div class="contact-item">
+                    <div class="icon">
+                      <img src="/icons/phone-1.svg" alt="Phone" />
+                    </div>
+                    <div class="info">
+                      <h3>TELEFONE</h3>
+                      <p>(+351) 218 386 856</p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </grid>
-        </div>
-      </section>
-    </div>
-  </main>
-  @include('footer')
+            </grid>
+          </div>
+        </section>
+      </div>
+    </main>
+    @include('footer')
+  </div>
   <script src="/js/nav.js" async defer></script>
 </body>
 
